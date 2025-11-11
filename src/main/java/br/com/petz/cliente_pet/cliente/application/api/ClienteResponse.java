deaaa9a803df5553ce70938d751f357c39914850
Roberto Pedro <robertopedro100@gmail.com>
2025-11-11 19:09:59 -0300

    Implementa Cliente service, inicia infra

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteResponse.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteResponse.java
@@ -1,9 +1,11 @@
 package br.com.petz.cliente_pet.cliente.application.api;
 
+import lombok.Builder;
 import lombok.Value;
 
 import java.util.UUID;
 @Value
+@Builder
 public class ClienteResponse {
     private UUID idCliente;
 
---
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/repository/ClienteRepository.java
@@ -0,0 +1,8 @@
+package br.com.petz.cliente_pet.cliente.application.repository;
+
+import br.com.petz.cliente_pet.cliente.domain.Cliente;
+
+public interface ClienteRepository {
+
+    Cliente salva(Cliente cliente);
+}
