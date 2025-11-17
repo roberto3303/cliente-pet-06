3c7504e86b379d13eb9f134061a66c625f87a630
Roberto Pedro <robertopedro100@gmail.com>
2025-11-17 15:17:22 -0300

    Implementa fluxo api, teste ok!

---
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteListResponse.java
@@ -0,0 +1,14 @@
+package br.com.petz.cliente_pet.cliente.application.api;
+
+
+
+import java.util.UUID;
+
+public class ClienteListResponse {
+    private UUID idCliente;
+    private String nomeCompleto;
+    private String email;
+    private String celular;
+    private String cpf;
+
+}
