24fa2ec2fd7e410a6425d348abc538ed6f012749
Roberto Pedro <robertopedro100@gmail.com>
2025-11-19 20:54:04 -0300

    Implementa Getbyid, Implementa Handler ao projeto

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteDetalhadoResponse.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteDetalhadoResponse.java
@@ -1,5 +1,6 @@
 package br.com.petz.cliente_pet.cliente.application.api;
 
+import br.com.petz.cliente_pet.cliente.domain.Cliente;
 import br.com.petz.cliente_pet.cliente.domain.Sexo;
 import lombok.Value;
 
@@ -19,5 +20,17 @@ public class ClienteDetalhadoResponse {
     private String telefone;
 
     private LocalDateTime dataHoraDoCadastro;
+
+    public ClienteDetalhadoResponse(Cliente cliente) {
+        this.idCliente = cliente.getIdCliente();
+        this.nomeCompleto = cliente.getNomeCompleto();
+        this.email = cliente.getEmail();
+        this.celular = cliente.getCelular();
+        this.cpf = cliente.getCpf();
+        this.sexo = cliente.getSexo();
+        this.aceitaTermos = cliente.getAceitaTermos();
+        this.telefone = cliente.getTelefone();
+        this.dataHoraDoCadastro = cliente.getDataHoraDoCadastro();
+    }
 
 }
