06a289d32918ff66100d63689a75ae52e76d6397
Roberto Pedro <robertopedro100@gmail.com>
2025-11-27 18:00:56 -0300

    Implementa fluxo do service

--- src/main/java/br/com/petz/cliente_pet/pet/application/api/PetController.java
+++ src/main/java/br/com/petz/cliente_pet/pet/application/api/PetController.java
@@ -28,7 +28,8 @@ public class PetController implements PetAPI {
     public List<PetClienteListResponse> getPetsDoClienteComId(UUID idCliente) {
         log.info("[inicia] PetController - getPetsDoClienteComId");
         log.info("[idCliente] {}", idCliente);
+        List<PetClienteListResponse> petsDoCliente = petService.buscaPetsDoClienteComId(idCliente);
         log.info("[finaliza] PetController - getPetsDoClienteComId");
-        return List.of();
+        return petsDoCliente;
     }
 }
--- src/main/java/br/com/petz/cliente_pet/pet/application/service/PetApplicationService.java
+++ src/main/java/br/com/petz/cliente_pet/pet/application/service/PetApplicationService.java
@@ -1,6 +1,7 @@
 package br.com.petz.cliente_pet.pet.application.service;
 
 import br.com.petz.cliente_pet.cliente.application.service.ClienteService;
+import br.com.petz.cliente_pet.pet.application.api.PetClienteListResponse;
 import br.com.petz.cliente_pet.pet.application.api.PetRequest;
 import br.com.petz.cliente_pet.pet.application.api.PetResponse;
 import br.com.petz.cliente_pet.pet.application.repository.PetRepository;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -27,5 +29,14 @@ public class PetApplicationService implements PetService {
         Pet pet = petRepository.salvaPet(new Pet(idCliente, petRequest));
         log.info("[finaliza] PetApplicationService - criaPet");
         return new PetResponse(pet.getIdPet());
+    }
+
+    @Override
+    public List<PetClienteListResponse> buscaPetsDoClienteComId(UUID idCliente) {
+        log.info("[inicia] PetApplicationService - buscaPetsDoClienteComId");
+        clienteService.buscaClienteAtravesId(idCliente);
+        
+        log.info("[finaliza] PetApplicationService - buscaPetsDoClienteComId");
+        return List.of();
     }
 }
